36030267b1476f6c0091e6e5528f11245ce8451c
wzj0503 <70878597+wzj0503@users.noreply.github.com>
2020-09-13 20:33:25 +0800

    Add files via upload

---
+++ Example01.java
@@ -0,0 +1,23 @@
+﻿public class Example01 {
+	public static void main(String[] args) {
+		int[] arr={9,8,3,5,2};
+		for (int i=0;i<arr.length;i++){
+			System.out.print(arr[i]+" ");
+		}
+		System.out.println();				
+		for(int i=1;i<arr.length;i++){
+			
+			for(int j=0;j<arr.length-i;j++){
+				if(arr[j]>arr[j+1]){	
+					int temp=arr[j];
+					arr[j]=arr[j+1];
+					arr[j+1]=temp;
+				}
+			}
+		}
+		
+		for(int i=0;i<arr.length;i++){
+			System.out.print(arr[i]+" ");
+		}
+	}
+}
