adc6069170f84e369e37b3f9e26f37aa34eae180
wzj0503 <70878597+wzj0503@users.noreply.github.com>
2020-09-15 18:49:27 +0800

    Update Demo01.java

--- Demo01.java
+++ Demo01.java
@@ -1,23 +1,16 @@
-﻿public class Example01 {
-	public static void main(String[] args) {
-		int[] arr={12,6,8,9,3};
-		for (int i=0;i<arr.length;i++){
-			System.out.print(arr[i]+" ");
-		}
-		System.out.println();				
-		for(int i=1;i<arr.length;i++){
-			
-			for(int j=0;j<arr.length-i;j++){
-				if(arr[j]>arr[j+1]){	
-					int temp=arr[j];
-					arr[j]=arr[j+1];
-					arr[j+1]=temp;
+
+import java.util.Arrays;
+
+public class Demo01 {
+	public static void mppx(int arr[]){
+		for (int i = 0; i <= arr.length - 1; i++){
+			for (int j = 0; j < arr.length - 1 - i;j++){
+				if (arr[j] > arr[j+1]){
+					int temp = arr[j];
+					arr[j] = arr[j+1];
+					arr[j+1] = temp;
 				}
 			}
-		}
-		
-		for(int i=0;i<arr.length;i++){
-			System.out.print(arr[i]+" ");
 		}
 	}
 }
